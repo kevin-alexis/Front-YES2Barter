f5f75825dce0f95ede73ff456114ba443315e31e
Jacob0418 <cjosecoronado97@gmail.com>
2025-03-09 15:45:24 -0500

    table primevue

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
 import { createPinia } from 'pinia'
+import Lara from '@primeuix/themes/lara';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  theme: {
+      preset: Lara,
+      options: {
+        darkModeSelector: false || 'none',
+    }
+  }
+});
 app.use(createPinia())
 app.use(router)
 
